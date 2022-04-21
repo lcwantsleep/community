c115154f8142702fe2a62049b9da8eef85bb11a9
xu <415140098@qq.com>
2022-04-21 22:42:05 +0800

    first commit

---
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -0,0 +1,44 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTests {
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+    @Autowired
+    private UserMapper userMapper;
+    @Test
+    public void testSelectUser() {
+        User user = userMapper.selectById(101);
+        System.out.println(user);
+
+        user = userMapper.selectByName("liubei");
+        System.out.println(user);
+
+        user = userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testSelectPost(){
+        List<DiscussPost> list= discussPostMapper.selectDiscussPosts(149,0,10);
+        for (DiscussPost post:list){
+            System.out.println(post);
+        }
+        int rows=discussPostMapper.selectDiscussPostRows(149);
+        System.out.println(rows);
+    }
+}
